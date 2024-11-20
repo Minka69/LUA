de42175cde5034e31c7371a1c9ce2da6963c1986
Minka69 <marks.sislovs@gmail.com>
2024-11-20 23:15:11 +0200

    Added name, age inputs, division and something else

--- code.lua
+++ code.lua
@@ -1,11 +1,32 @@
-name = "Mark"
-age = "16"
-a = (7)
-b = (5)
-c = (a * b)
-d = (b + c)
-e = (c + d)
-f = (d + e)
+print("Enter your name: ")
+name = io.read()
+
+print("Enter your age: ")
+age = tonumber(io.read())
+
+
+print("Enter the first number:")
+a = tonumber(io.read())
+
+print("Enter the second number:")
+b = tonumber(io.read())
+
+if not a or not b then
+    print("Invalid input for numbers! Please enter numbers only.")
+    return
+end
+
+
+c = a * b
+d = b * c
+e = c * d
+f = d * e
+
 sum = (a + b + c + d + e + f)
 multiplication = (a * b * c * d * e * f)
-print("Hello " .. name .. ", you are " .. age .. " years old! " .. "sum of your number are: " .. sum .. " and multiplication: " .. multiplication .. "!")
+division = (a / b / c / d / e / f)
+
+print("Hello " .. name .. ", you are " .. age .. " years old!")
+print("The sum of your numbers are: " .. sum .. "!")
+print("The multiplication of your numbers are: " .. multiplication .. "!")
+print("The division of your numbers are: " .. division .. "!")
